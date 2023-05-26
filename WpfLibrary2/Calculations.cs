f0ebbfcb6abdae623728378e651506db32cd8c53
R2 <R2@MILA-SHKA>
2023-05-26 10:31:33 +0400

    исправка времени

--- WpfLibrary2/Calculations.cs
+++ WpfLibrary2/Calculations.cs
@@ -18,8 +18,34 @@ namespace WpfLibrary2
         /// <param name="consultationTime"> время на консультацию</param>
         /// <returns> возвращает список свободных временных интервалов в графике сотрудника</returns>
         public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime,
-            TimeSpan endWorkingTime, int consultationTime){}
+            TimeSpan endWorkingTime, int consultationTime)
+        {
+            List<string> result = new List<string>();
+            if (endWorkingTime <= beginWorkingTime)
+                endWorkingTime += TimeSpan.FromHours(24);
+
+            var consultationSlotDuration = TimeSpan.FromMinutes(consultationTime);
+            var workStart = beginWorkingTime;
+            var breaks = startTimes
+            .Select(startTime => startTime <= beginWorkingTime ? startTime + TimeSpan.FromHours(24) : startTime)
+            .Zip(durations, (start, duration) => (start, end: start + TimeSpan.FromMinutes(duration)))
+            .Append((endWorkingTime, TimeSpan.Zero));
+
+            foreach (var (breakStart, breakEnd) in breaks)
+            {
+                var slotCount = (int)((breakStart - workStart) / consultationSlotDuration);
+                for (int j = 0; j < slotCount; j++)
+                {
+                    var slotStart = workStart + consultationSlotDuration * j;
+                    var slotEnd = workStart + consultationSlotDuration * (j + 1);
+                    result.Add($"{slotStart:hh':'mm}-{slotEnd:hh':'mm}");
+                }
+                workStart = breakEnd;
+            }
+            return result.ToArray();
+        }
     }
+    
 }
 
     
--- WpfLibrary2Tests/CalculationTests.cs
+++ WpfLibrary2Tests/CalculationTests.cs
@@ -2,13 +2,20 @@
 
 namespace WpfLibrary2.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class CalculationTests
     {
+        
         [TestMethod()]
         public void AvailablePeriodsTest()
         {
-            Assert.Fail();
+            TimeSpan[] startTime = new TimeSpan[] { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0),
+                new TimeSpan(15, 30, 0), new TimeSpan(16, 50, 0) };
+            int[] duration = { 60, 30, 10, 10, 40 };
+            string[] actual = Calculation.AvailablePeriods(startTime, duration, new TimeSpan(8, 0, 0), new TimeSpan(18, 0, 0), 30);
+            string[] expected = new string[] { "08:00-08:30", "08:30-09:00", "08:00-08:30", "09:00-09:30", "09:30-10:00",
+                "11:30-12:00", "12:30-13:00", "13:00-13:30", "13:30-14:00", "14:00-14:30", "14:30-15:00", "15:40-16:10", "16:10-16:40", "17:30-18:00" };
+            CollectionAssert.Equals(expected, actual);
         }
                
     }
